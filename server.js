851e91f744998f0fd8c44c543f245b62e74af557
Ishan Dilmith <168458597+IshanDilmith@users.noreply.github.com>
2026-03-21 12:40:49 +0530

    feat: Add OpenAPI specification and integrate Swagger UI for API documentation.

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./src/config/db');
-// const swaggerUi = require('swagger-ui-express');
-// const swaggerDocument = require('./swagger.json');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const app = express();
 app.use(express.json());
@@ -14,7 +14,7 @@ connectDB();
 app.use('/orders', require('./src/routes/orders'));
 
 // Swagger docs
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
